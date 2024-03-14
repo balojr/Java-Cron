313c40c2e69b053acef0619c4071f695d7266365
balojr <martinkagendo13@gmail.com>
2024-03-14 13:45:03 +0300

    add configs

---
+++ src/test/java/com/slime/cronjobs/CronjobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slime.cronjobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CronjobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
