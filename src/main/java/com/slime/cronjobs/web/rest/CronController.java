18d9db3e3e57630d72825cb67ecfdbdd7dfcc2f3
balojr <martinkagendo13@gmail.com>
2024-03-14 13:44:55 +0300

    add configs

---
+++ src/main/java/com/slime/cronjobs/web/rest/CronController.java
@@ -0,0 +1,11 @@
+package com.slime.cronjobs.web.rest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/cron")
+public class CronController {
+}
