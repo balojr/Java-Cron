92824a5ed2d56973ca6df7ca1be4fbaee0b32829
balojr <martinkagendo13@gmail.com>
2024-03-14 14:18:50 +0300

    add configs

--- src/main/java/com/slime/cronjobs/config/CronConfig.java
+++ src/main/java/com/slime/cronjobs/config/CronConfig.java
@@ -4,24 +4,73 @@ import com.slime.cronjobs.service.CronService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
+
+ /**
+ * CronConfig class.
+ */
 @Configuration
 @EnableScheduling
 @Slf4j
+@EnableAsync
 public class CronConfig {
 
     private final CronService cronService;
 
+    /**
+     * Constructor for CronConfig.
+     *
+     * @param cronService the CronService to be used by this CronConfig
+     */
     @Autowired
     public CronConfig(CronService cronService) {
         this.cronService = cronService;
     }
 
+        /**
+     * Scheduled task that is executed every 2000 milliseconds.
+     * It logs the current time and executes the cron job.
+     */
     @Scheduled(fixedDelay = 2000)
     public void scheduleFixedDelayTask() {
-        log.info("Fixed delay task - " + System.currentTimeMillis() / 2000);
+        log.info("Fixed delay task - " + System.currentTimeMillis() / 1000);
         cronService.executeCronJob();
     }
+
+    /**
+     * Scheduled task that is executed at a fixed rate every 2000 milliseconds.
+     * It logs the current time and executes the second cron job.
+     */
+    @Scheduled(fixedRate = 2000)
+    public void scheduleFixedRateTask() {
+        log.info("Fixed rate task - " + System.currentTimeMillis() / 2000);
+        cronService.executeCronJob2();
+    }
+
+    /**
+     * Asynchronously scheduled task that is executed at a fixed rate every 1000 milliseconds.
+     * It logs the current time and then sleeps for 2000 milliseconds.
+     * @throws InterruptedException if the thread sleep is interrupted
+     */
+    @Async
+    @Scheduled(fixedRate = 1000)
+    public void scheduleFixedRateTaskAsync() throws InterruptedException {
+        log.info("Fixed rate task async - " + System.currentTimeMillis() / 1000);
+        Thread.sleep(2000);
+    }
+
+    /**
+     * Scheduled task that is executed using a cron expression.
+     * It logs the current time. The cron expression is set to trigger the task every day at 11:56 PM.
+     */
+    @Scheduled(cron = "0 55 23 * * ?")
+    public void scheduleTaskUsingCronExpression() {
+        long now = System.currentTimeMillis() / 1000;
+        log.info("schedule tasks using cron jobs - " + now);
+    }
 }
+
--- src/main/java/com/slime/cronjobs/service/CronService.java
+++ src/main/java/com/slime/cronjobs/service/CronService.java
@@ -4,14 +4,42 @@ import com.slime.cronjobs.config.CronConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+/**
+ * CronService class.
+ */
 @Service
 @Slf4j
 public class CronService {
-    private static final String CRON_JOB = "Cron Job Executed Successfully!!!";
+    private static final String CRON_JOB_1 = "Cron Job Executed Successfully!!!";
 
+    private static final String CRON_JOB_2 = "Cron Job 2 Executed Successfully!!!";
+
+    /**
+     * Executes the cron job and logs its success.
+     *
+     * @return a string message indicating the successful execution of the cron job
+     */
     public String executeCronJob() {
-        log.info(CRON_JOB);
-        return CRON_JOB;
+        log.info(CRON_JOB_1);
+        return CRON_JOB_1;
     }
 
+        /**
+     * Executes the second cron job and logs its success.
+     *
+     * @return a string message indicating the successful execution of the second cron job
+     */
+    public String executeCronJob2() {
+        log.info(CRON_JOB_2);
+        return CRON_JOB_2;
+    }
+
+    /**
+     * Returns the delay for the next execution of the cron job.
+     *
+     * @return a long value representing the delay in milliseconds
+     */
+    public long getDelay() {
+        return 2000L; // returns a fixed delay of 2000 milliseconds
+    }
 }
