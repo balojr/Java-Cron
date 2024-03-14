1c0591bba8305928fe334a14f9909e5ab64d8cec
balojr <martinkagendo13@gmail.com>
2024-03-14 13:44:57 +0300

    add configs

---
+++ src/main/java/com/slime/cronjobs/CronjobsApplication.java
@@ -0,0 +1,13 @@
+package com.slime.cronjobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CronjobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CronjobsApplication.class, args);
+	}
+
+}
